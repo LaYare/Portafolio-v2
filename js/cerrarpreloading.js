4cb83c8ae91d87a2142b52d228a775f7b1c0c69d
YareArista <yarearista@gmail.com>
2016-11-30 08:45:45 -0600

    Se agregan mejoras al portafolio, boton que va hacia arriba, links y gif

--- js/cerrarpreloading.js
+++ js/cerrarpreloading.js
@@ -1,10 +1,5 @@
 var container = document.getElementById('conte');
-var body = document.getElementById('bod');
 
 setTimeout(function(){
     container.classList.add('cerrar');
-    body.classList.add('bod');
-
-
 },4500);
-
