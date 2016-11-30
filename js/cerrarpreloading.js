163903e2e0b6ca326ebf272c507ffb428f7a9252
YareArista <yarearista@gmail.com>
2016-11-29 19:19:41 -0600

    Segunda version de portafolio

---
+++ js/cerrarpreloading.js
@@ -0,0 +1,10 @@
+var container = document.getElementById('conte');
+var body = document.getElementById('bod');
+
+setTimeout(function(){
+    container.classList.add('cerrar');
+    body.classList.add('bod');
+
+
+},4500);
+
